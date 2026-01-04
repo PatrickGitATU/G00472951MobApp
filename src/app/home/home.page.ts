5b89244e19a0799268249095b3799dd211bb5550
PatrickGitATU <G00472951@atu.ie>
2026-01-04 01:32:46 +0000

    Update changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,81 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';  //Controls page routing throughout the application
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { DataService }  from '../services/data.service' ; 
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
+import { CommonModule } from '@angular/common'; // needed for ngFor 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-   imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent,  FormsModule ] ,
+   imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,CommonModule] ,
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-ingredients: string = "";
+elementIsHidden: boolean = true; //hidden initially
+ingredients:string = "";
+ingredientsInfo!:any;
 
-  constructor() {}
+//https://api.spoonacular.com/recipes/complexSearch?apiKey=70759a4f7911402abcc53d3c51d3b759&query=rice
+
+apiKey= "70759a4f7911402abcc53d3c51d3b759"    
 
 
-  showRecipesForXYZ(){
-    
+options: HttpOptions = { 
+
+url: "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + this.apiKey + "&query="
+  }
+
+  
+
+
+
+//create an instance of router,create an instance for the data service, 
+  constructor(private router: Router, private ds: DataService, private mhs:MyHttpService) { }
+  
+  
+  ngOnInit() {
+
+this.getKW();
+   
+  }
+
+//this.ingredients = "ingredients";
+
+  async getKW(){ 
+
+ this.ingredients =  await this.ds.get('kw'); 
+
+
+ this.options.url = this.options.url.concat(this.ingredients)
+ let result = await this.mhs.get(this.options)
+ this.ingredientsInfo = result.data.results;
+
+ // this.mhs.get(this.options)
+  //this.ingredientsInfo  = await this.mhs.get(this.options)
+  //this.ingredientsInfo = result.data.Search;
+
+//console.log(this.ingredientsInfo) ///displays the data in console with the "results" field
+ console.log(JSON.stringify(this.ingredientsInfo))
+
+
+}
+
+
+
+
+
+  async showRecipesForXYZ(){
+
+   await  this.ds.set("kw", this.ingredients) //value in storage will be arg1 (arg1, arg2)
+   this.elementIsHidden = !this.elementIsHidden;  //toggle the element to vible when the button is pressed
+   
+    // this.ingredients = "";
+   //this.router.navigate(['/recipe-details']);
+  
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Storage  } from '@ionic/storage-angular';
+
+
+
+
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class DataService {
+export class DataService { 
   
+  constructor(private storage: Storage){
+
+    this.init();
+   }
+
+//creates the storage
+async init(){    
+  await this.storage.create(); 
+
 }
+
+//method to place the values into storage
+async set(key: string, value: any){
+
+  await this.storage.set(key, value); 
+}
+
+
+//method to get the values from storage
+async get(key: string){
+
+  return   await this.storage.get(key); 
+}
+
+
+  }
