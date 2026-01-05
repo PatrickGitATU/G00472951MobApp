98bf333232e7b38d392d2daf0f401ca6e59b15ab
PatrickGitATU <G00472951@atu.ie>
2026-01-05 01:55:03 +0000

    Update changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -59,17 +59,25 @@ url: "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + this.apiKey +
 
  this.ingredients =  await this.ds.get('kw'); 
 
+ console.log("this.ingredients in async getKW() is " + this.ingredients)
 
- this.options.url = this.options.url.concat(this.ingredients)
- let result = await this.mhs.get(this.options)
- this.ingredientsInfo = result.data.results;
+ //this.options.url = this.options.url.concat(this.ingredients)  //not needed here, updated in showRecipesForXYZ()
+ //console.log("ingredients search url in getKW() is " + this.options.url) //not needed here, updated in showRecipesForXYZ()
+
+// let result = await this.mhs.get(this.options)  //commented until i can see where url is getting the right value
+// this.ingredientsInfo = result.data.results;  //commented until i can see where url is getting the right value
+
+
+
 
  // this.mhs.get(this.options)
   //this.ingredientsInfo  = await this.mhs.get(this.options)
   //this.ingredientsInfo = result.data.Search;
 
+
+
 //console.log(this.ingredientsInfo) ///displays the data in console with the "results" field
- console.log(JSON.stringify(this.ingredientsInfo))
+ //console.log(JSON.stringify(this.ingredientsInfo))
 
 
 }
@@ -80,17 +88,39 @@ url: "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + this.apiKey +
 
   async showRecipesForXYZ(){
 
-   await  this.ds.set("kw", this.ingredients) //value in storage will be arg1 (arg1, arg2)
-   this.elementIsHidden = !this.elementIsHidden;  //toggle the element to vible when the button is pressed
+    //hide-show main card of results on home page
+    this.elementIsHidden = !this.elementIsHidden;  //toggle the element to visible when the button is pressed
+
+    console.log("this.ingredients in   async showRecipesForXYZ() is = "+ this.ingredients)
+    
+   //set the value for kw from what was entered
+   await  this.ds.set("kw", this.ingredients); //value in storage will be arg1 (arg1, arg2)
+    
+   //update the url with the value entered into the search
+   this.options.url = this.options.url.concat(this.ingredients)
+    console.log("ingredients search url in showRecipesForXYZ() is " + this.options.url)
+ 
+   //send the the query 
+    let result = await this.mhs.get(this.options)
+
+    //retrieve the query results
+    this.ingredientsInfo = result.data.results;
+
+
+   
    
     // this.ingredients = "";
    //this.router.navigate(['/recipe-details']);
+
+   //console.log(this.ingredientsInfo) ///displays the data in console with the "results" field
+ console.log(JSON.stringify(this.ingredientsInfo))
   
   }
 
 
 async goToRecipeDetails(idPassedFromIngredientObject: string ) {
-this.ingredientID=idPassedFromIngredientObject;
+ this.ingredientID = idPassedFromIngredientObject;
+console.log("goToRecipeDetails has ID = "+ idPassedFromIngredientObject)
  await  this.ds.set("ingrdntID", this.ingredientID) //value in storage will be arg1 (arg1, arg2)
 
 this.router.navigate(['/recipe-details']);
--- src/app/recipe-details/recipe-details.page.ts
+++ src/app/recipe-details/recipe-details.page.ts
@@ -3,30 +3,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';  //Controls page routing throughout the application
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonList, IonItem, IonLabel, IonImg } from '@ionic/angular/standalone';
 import { DataService }  from '../services/data.service' ; 
 import { MyHttpService } from '../services/my-http.service';
 import { HttpOptions } from '@capacitor/core';
 import { CommonModule } from '@angular/common'; // needed for ngFor 
+
+//import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-   imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,CommonModule] ,
+   imports: [IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, CommonModule, IonList, IonItem, IonLabel] ,
 })
 
 export class RecipeDetailsPage implements OnInit{
 ingrdntIDHere: string = "";
 apiKey= "70759a4f7911402abcc53d3c51d3b759";
 
+ingredientsFullDetails!:any;
+
+  extendedIngredients:any = [];
+  measures:any = [];
+
 
 //create an instance of router,create an instance for the data service, 
   constructor(private routerRecipeDetails: Router, private dsRecipeDetails: DataService, private mhsRecipeDetails:MyHttpService) { }
 
 
-
+//The Final URL is updated with the new value for "this.ingrdntIDHere" in "async getIngrdntInfo()"
+//URL is not complete and only initialsed here
 optionsHere: HttpOptions = { 
 
 url: "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information?" + "apiKey="  + this.apiKey 
@@ -42,7 +50,8 @@ url: "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information
   
    await this.getIngrdntID(); // Fetch updated  value
   
-   
+  await  this.getIngrdntInfo();  //fetch full recipe details 
+
   }
 
 //elementIsHidden: boolean = true; //hidden initially
@@ -60,7 +69,7 @@ url: "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information
 
  this.ingrdntIDHere =  await this.dsRecipeDetails.get('ingrdntID'); 
 
-  console.log("Id is " + this.ingrdntIDHere)
+  console.log("Id for ingrdntIDHere is " + this.ingrdntIDHere)
 
  //this.options.url = this.options.url.concat(this.ingredients)
  //let result = await this.mhs.get(this.options)
@@ -68,9 +77,32 @@ url: "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information
 
 
 //console.log(this.ingredientsInfo) ///displays the data in console with the "results" field
+
  //console.log(JSON.stringify(this.ingredientsInfo))
 
 
+  }
+
+
+async getIngrdntInfo()
+
+{
+ //this.ingredients =  await this.ds.get('kw'); 
+
+ 
+console.log("Id for ingrdntIDHere in getIngrdntInfo() is " + this.ingrdntIDHere) 
+ this.optionsHere.url = "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information?apiKey=" + this.apiKey;
+  console.log("URL get request is now " + this.optionsHere.url)
+
+ //this.optionsHere.url = this.optionsHere.url
+ 
+  let resultHere = await this.mhsRecipeDetails.get(this.optionsHere)  //
+ this.ingredientsFullDetails = resultHere.data;
+this.extendedIngredients =  resultHere.data.extendedIngredients;
+this.measures = resultHere.data.extendedIngredients.measures;
+//console.log(this.ingredientsFullDetails) ///displays the object  in console with the "results" field
+ console.log("this is ingredientsFullDetails" + JSON.stringify(this.ingredientsFullDetails))  //displays the data
+//console.log(JSON.stringify("this is extendedIngredients" + this.extendedIngredients))
 }
 
 
@@ -81,4 +113,7 @@ url: "https://api.spoonacular.com/recipes/" + this.ingrdntIDHere + "/information
 
 
 
+
+
 }
+
